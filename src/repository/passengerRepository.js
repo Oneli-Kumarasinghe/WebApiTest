292d6b41593436afb7761401a1ce956bbee99bf3
Oneli-Kumarasinghe <onelisaku@gmail.com>
2024-12-14 16:47:52 +0530

    changes settingup

--- src/repository/passengerRepository.js
+++ src/repository/passengerRepository.js
@@ -1,11 +1,15 @@
 const sequelize = require('../config/database');
 
-class PassengerRepository{
-    async createPassenger(passenger){
-    const query = 'INSERT INTO passenger (nic_no, full_name, address, contact_info, email, date_of_birth, password) VALUES ($1, $2, $3, $4, $5, $6) RETURNING nic_no';
-    const values = [passenger.nic_no, passenger.full_name, passenger.address, passenger.contact_info, passenger.email, passenger.date_of_birth, passenger.password];        
-    const result = await sequelize.query(query,values);
-    return result.rows[0];
+class PassengerRepository {
+    async createPassenger(passenger) {
+        const query = 'INSERT INTO passenger (nic_no, full_name, address, contact_info, email, date_of_birth, password) VALUES ($1, $2, $3, $4, $5, $6) RETURNING nic_no';
+        const values = [passenger.nic_no, passenger.full_name, passenger.address, passenger.contact_info, passenger.email, passenger.date_of_birth, passenger.password];
+        const result = await sequelize.query(query, {
+            replacements: values,
+            type: sequelize.QueryTypes.INSERT,
+        });
+        return result[0]; // Sequelize returns the result as an array
     }
 }
+
 module.exports = new PassengerRepository();
