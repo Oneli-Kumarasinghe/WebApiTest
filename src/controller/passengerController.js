423b74f9463278740659ee695b1f0cc311e3826f
Oneli-Kumarasinghe <onelisaku@gmail.com>
2024-12-14 16:30:29 +0530

    changes settingup

--- src/controller/passengerController.js
+++ src/controller/passengerController.js
@@ -4,7 +4,7 @@ class PassengerController {
   async passengerRegistration(req, res) {
     try {
       const { nic_no, full_name, address, contact_info, email,date_of_birth, password } = req.body;
-      const passengerNew = await passengerService.registerPassenger(nic_no, full_name, address, contact_info, email,date_of_birth, password);
+      const passengerNew = await passengerService.passengerRegistration(nic_no, full_name, address, contact_info, email,date_of_birth, password);
       res.status(201).json({ message: 'Passenger registered successfully', newPassenger: newPassenger.nic_no });
     } catch (error) {
       res.status(400).json({ error: error.message });
