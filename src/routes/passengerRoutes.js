691173098fef4a76ceb2644572e37fa464df1856
Oneli-Kumarasinghe <onelisaku@gmail.com>
2024-12-14 15:46:39 +0530

    changes in settingup

--- src/routes/passengerRoutes.js
+++ src/routes/passengerRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const PassengerController = require('../controller/passengerController');
-const PassengerController = require('../controller/passengerController');
 
 const router = express.Router();
 router.post('/register', PassengerController.registerPassenger);
