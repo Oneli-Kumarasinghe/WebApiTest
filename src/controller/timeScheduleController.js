bb2f4819eb3dc5b00aed6eb7871a7a0aab574902
Oneli-Kumarasinghe <onelisaku@gmail.com>
2024-12-22 16:58:32 +0530

    changed settingup

--- src/controller/timeScheduleController.js
+++ src/controller/timeScheduleController.js
@@ -5,7 +5,7 @@ class timeScheduleController {
         try {
               const {route_id} = req.body;
               const schedulesOfAll = await timeScheduleService.findAllSchedulesWithRouteId(route_id);
-              res.status(201).json({ message: 'Data successfully fetched', schedulesOfAll: schedulesOfAll.route_id });
+              res.status(201).json({ message: 'Data successfully fetched', schedulesOfAll: schedulesOfAll });
             } catch (error) {
               res.status(500).json({ error: error.message });
             }
