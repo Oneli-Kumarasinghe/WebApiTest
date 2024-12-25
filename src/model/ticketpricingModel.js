ed262d198ad3142303a90ebd72d2a4f3370af072
Oneli-Kumarasinghe <onelisaku@gmail.com>
2024-12-25 21:57:30 +0530

    changed settingup

--- src/model/ticketpricingModel.js
+++ src/model/ticketpricingModel.js
@@ -1,6 +1,6 @@
 const {DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
-const Buses = require('./busesModel');
+const busesModel = require('./busesModel');
 
 
 
