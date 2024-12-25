84a806b35ce9bad217a3d5a101d39f38c1b9f2fc
Oneli-Kumarasinghe <onelisaku@gmail.com>
2024-12-25 16:25:03 +0530

    changed settingup

--- src/routes/ticketpricingRoutes.js
+++ src/routes/ticketpricingRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ticketpricingService = require('../services/ticketpricingService');
+const ticketpricingService = require('../service/ticketpricingService');
 
 // Endpoint to get filtered buses
 router.get('/filtered-buses', async (req, res) => {
