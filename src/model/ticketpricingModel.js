dcc83234048d2dad828ed3bc087e148ba2b26da3
Oneli-Kumarasinghe <onelisaku@gmail.com>
2024-12-25 20:53:18 +0530

    changed settingup

--- src/model/ticketpricingModel.js
+++ src/model/ticketpricingModel.js
@@ -2,23 +2,23 @@ const {DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 
-const Ticket_price = sequelize.define('ticket_price',{
+const ticketpricingModel = sequelize.define('ticketpricingModel',{
     ticket_id: {
         type: DataTypes.STRING,
         primaryKey: true,
       },
       bus_type: DataTypes.STRING,
-      ticket_price : DataTypes.FLOAT,
+      ticketpricingModel : DataTypes.FLOAT,
     }, {
-      tableName: 'ticket_price',
+      tableName: 'ticketpricingModel',
       timestamps: false,
     });
     // Define Buses model
     const Buses = require('./busesModel');
 
     // Association: Ticket_price belongs to Buses by bus_type
-    Ticket_price.belongsTo(Buses, {
+    ticketpricingModel.belongsTo(Buses, {
     foreignKey: 'bus_type',
     targetKey: 'type', // 'type' in Buses model
     });
-    module.exports = Ticket_price;
+    module.exports = ticketpricingModel;
