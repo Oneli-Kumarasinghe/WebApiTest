6ef201c539e620196720a649a1e299ded67ecff4
Oneli-Kumarasinghe <onelisaku@gmail.com>
2024-12-26 21:36:21 +0530

    changed settingup

--- src/routes/busesRoutes.js
+++ src/routes/busesRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const busController = require('../controller/busesController');
 
-router.post('/filtered-buses', busController.getFilteredBusesWithPrices);
+router.get('/filtered-buses', busController.getFilteredBusesWithPrices);
 
 module.exports = router;
