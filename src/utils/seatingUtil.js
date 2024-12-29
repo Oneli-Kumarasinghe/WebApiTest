886b0bf0e76f163273bbfd8bd8068a7bbe0f3a00
Oneli-Kumarasinghe <onelisaku@gmail.com>
2024-12-29 23:28:50 +0530

    changed settingup

--- src/utils/seatingUtil.js
+++ src/utils/seatingUtil.js
@@ -3,21 +3,21 @@ const { Double_Decker, Coach, Mini_Bus } = require('./seats');
 
 class seatingUtils {
   async DoubleDeckerFiltering(BookSeats = []) {
-    
-    const filteredSeats = Double_Decker.filter(allseats => !BookSeats.includes(allseats.seatNumber));
-    return filteredSeats;
+      const bookedSeatNumbers = BookSeats.map(seat => seat.seatNumber); 
+      const filteredSeats = Double_Decker.filter(allseat => !bookedSeatNumbers.includes(allseat.seatNumber));
+      return filteredSeats;
   }
 
   async CoachFiltering(BookSeats = []) {
-    
-    const filteredSeats = Coach.filter(allseats => !BookSeats.includes(allseats.seatNumber));
-    return filteredSeats;
+      const bookedSeatNumbers = BookSeats.map(seat => seat.seatNumber);
+      const filteredSeats = Coach.filter(allseat => !bookedSeatNumbers.includes(allseat.seatNumber));
+      return filteredSeats;
   }
 
   async MiniBusFiltering(BookSeats = []) {
-    
-    const filteredSeats = Mini_Bus.filter(allseats => !BookSeats.includes(allseats.seatNumber));
-    return filteredSeats;
+      const bookedSeatNumbers = BookSeats.map(seat => seat.seatNumber);
+      const filteredSeats = Mini_Bus.filter(allseat => !bookedSeatNumbers.includes(allseat.seatNumber));
+      return filteredSeats;
   }
 }
 
