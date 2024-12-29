283d2198aa4343beb7cd499738852db5622e70fc
Oneli-Kumarasinghe <onelisaku@gmail.com>
2024-12-29 23:51:10 +0530

    Update seatingUtil.js

--- src/utils/seatingUtil.js
+++ src/utils/seatingUtil.js
@@ -3,22 +3,19 @@ const { Double_Decker, Coach, Mini_Bus } = require('./seats');
 
 class seatingUtils {
   async DoubleDeckerFiltering(BookSeats = []) {
-      const bookedSeatNumbers = BookSeats.map(seat => seat.seatNumber); 
-      const filteredSeats = Double_Decker.filter(allseat => !bookedSeatNumbers.includes(allseat.seatNumber));
-      return filteredSeats;
-  }
+    const filteredSeats = Double_Decker.filter(allseat => !BookSeats.includes(allseat.seatNumber));
+    return filteredSeats;
+}
 
-  async CoachFiltering(BookSeats = []) {
-      const bookedSeatNumbers = BookSeats.map(seat => seat.seatNumber);
-      const filteredSeats = Coach.filter(allseat => !bookedSeatNumbers.includes(allseat.seatNumber));
-      return filteredSeats;
-  }
+async CoachFiltering(BookSeats = []) {
+    const filteredSeats = Coach.filter(allseat => !BookSeats.includes(allseat.seatNumber));
+    return filteredSeats;
+}
 
-  async MiniBusFiltering(BookSeats = []) {
-      const bookedSeatNumbers = BookSeats.map(seat => seat.seatNumber);
-      const filteredSeats = Mini_Bus.filter(allseat => !bookedSeatNumbers.includes(allseat.seatNumber));
-      return filteredSeats;
-  }
+async MiniBusFiltering(BookSeats = []) {
+    const filteredSeats = Mini_Bus.filter(allseat => !BookSeats.includes(allseat.seatNumber));
+    return filteredSeats;
+}
 }
 
 module.exports = new seatingUtils();
