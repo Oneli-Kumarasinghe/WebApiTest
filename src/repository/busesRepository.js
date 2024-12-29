d5c0a6744f43a95ccf243977eace560fa81ad6ad
Oneli-Kumarasinghe <onelisaku@gmail.com>
2024-12-29 21:11:35 +0530

    changed settingup

--- src/repository/busesRepository.js
+++ src/repository/busesRepository.js
@@ -1,6 +1,7 @@
 
 const { Buses, TicketPricing } = require('../model/associations'); 
 const busesModel = require('../model/busesModel');
+const { QueryTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 class BusRepository {
