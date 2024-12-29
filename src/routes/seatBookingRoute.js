52470a8b9475caf955a8ec9e12bc536837e1e420
Oneli-Kumarasinghe <onelisaku@gmail.com>
2024-12-29 17:56:28 +0530

    changed settingup

--- src/routes/seatBookingRoute.js
+++ src/routes/seatBookingRoute.js
@@ -3,4 +3,5 @@ const seatbookingController = require('../controller/seatbookingController');
 
 const routes = express.Router();
 routes.post('/saveSeats', seatbookingController.seatBooking);
+routes.get('/getSeatings',seatbookingController.getAvailableSeatings);
 module.exports = routes;
